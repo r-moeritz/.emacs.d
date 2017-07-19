51c7fc77729395d433fb52aa9caf4451f35c847f
Ralph Möritz <ralphmoritz@outlook.com>
2017-07-19 16:53:46 +0200

    Foiled by customsetfaces

--- init.el
+++ init.el
@@ -6,17 +6,4 @@
 
 (require 'setup-general)
 (require 'setup-packages)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (irfc elpy auto-complete zencoding-mode smex paredit web-mode modeline-posn markdown-mode log4j-mode htmlize expand-region ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
