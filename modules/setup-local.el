752e775fe5abe3101a6eb9c6e47b4b108ce333c5
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2022-01-02 23:00:07 +0100

    Use KickAssembler mode for files with .asm extension

--- modules/setup-local.el
+++ modules/setup-local.el
@@ -9,7 +9,7 @@
   (require 'kickasm-mode)
   (customize-set-variable 'kickasm-command "java -cp \"$HOME/Programs/KickAssembler/*\" kickass.KickAssembler")
   (customize-set-variable 'kickasm-assemble-command (concat kickasm-command " -vicesymbols -debugdump"))
-  (add-to-list 'auto-mode-alist '("\\.s\\'" . kickasm-mode)))
+  (add-to-list 'auto-mode-alist '("\\.\\(s\\|asm\\)$" . kickasm-mode)))
 
 (mapc 'funcall local-config-funcs)
 
